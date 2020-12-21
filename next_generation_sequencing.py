9849d16d7a6f3e4673becf19b7061c09a21dd0f3
sjlgg <3066268521@qq.com>
2020-12-21 12:13:30 +0800

    preprocessing changes

--- next_generation_sequencing.py
+++ next_generation_sequencing.py
@@ -24,25 +24,29 @@ class NextGenerationSequencing:
             if self.conf['preprocessing']['fastqc']['enable']:
                 os.mkdir(os.path.abspath('.') + '/' + self.result_dir + '/' + 'preprocessing/fastqc_before_filtering')
                 preprocessing_obj.fastqc_single_end('fastqc_before_filtering')
-            if self.conf['preprocessing']['trimmomatic']['enable']:
-                preprocessing_obj.trimmomatic_single_end()
-                self.preprocessing_output = self.result_dir+'/preprocessing/trimmomatic_output.fastq'
-                if self.conf['preprocessing']['fastqc']['enable']:
-                    preprocessing_obj.input_file = self.preprocessing_output
-                    os.mkdir(os.path.abspath('.') + '/' + self.result_dir + '/' + 'preprocessing/fastqc_after_filtering')
-                    preprocessing_obj.fastqc_single_end('fastqc_after_filtering')
-            elif self.conf['preprocessing']['cutadapt']['enable']:
-                preprocessing_obj.cutadapt_single_end()
-                self.preprocessing_output = self.result_dir+'/preprocessing/cutadapt_output.fastq'
-                if self.conf['preprocessing']['fastqc']['enable']:
-                    preprocessing_obj.input_file = self.preprocessing_output
-                    os.mkdir(os.path.abspath('.') + '/' + self.result_dir + '/' + 'preprocessing/fastqc_after_filtering')
-                    preprocessing_obj.fastqc_single_end('fastqc_after_filtering')
-            elif self.conf['preprocessing']['fastp']['enable']:
+
+            if self.conf['preprocessing']['fastp']['enable']:
                 preprocessing_obj.fastp_single_end()
                 self.preprocessing_output = self.result_dir+'/preprocessing/fastp_output.fastq'
             else:
-                self.preprocessing_output = self.input_file
+                if self.conf['preprocessing']['cutadapt']['enable']:
+                    preprocessing_obj.cutadapt_single_end()
+                    self.preprocessing_output = self.result_dir+'/preprocessing/cutadapt_output.fastq'
+                    if self.conf['preprocessing']['fastqc']['enable']:
+                        preprocessing_obj.input_file = self.preprocessing_output
+                        os.mkdir(os.path.abspath('.') + '/' + self.result_dir + '/' + 'preprocessing/fastqc_after_cutadapt_filtering')
+                        preprocessing_obj.fastqc_single_end('fastqc_after_cutadapt_filtering')
+                if self.conf['preprocessing']['trimmomatic']['enable']:
+                    if self.conf['preprocessing']['cutadapt']['enable']:
+                        preprocessing_obj.input_file = self.result_dir+'/preprocessing/cutadapt_output.fastq'
+                    preprocessing_obj.trimmomatic_single_end()
+                    self.preprocessing_output = self.result_dir+'/preprocessing/trimmomatic_output.fastq'
+                    if self.conf['preprocessing']['fastqc']['enable']:
+                        preprocessing_obj.input_file = self.preprocessing_output
+                        os.mkdir(os.path.abspath('.') + '/' + self.result_dir + '/' + 'preprocessing/fastqc_after_trimmomatic_filtering')
+                        preprocessing_obj.fastqc_single_end('fastqc_after_trimmomatic_filtering')
+                else:
+                    self.preprocessing_output = self.input_file
             temp_file = open(os.path.abspath('.') + '/' + self.result_dir+'/Summary_of_results.html', 'a+')
             temp_file.write('<ul>\n')
             temp_file.write('<li>preprocessing result is in %s</li>\n' % './preprocessing')
@@ -68,31 +72,36 @@ class NextGenerationSequencing:
             if self.conf['preprocessing']['fastqc']['enable']:
                 os.mkdir(os.path.abspath('.') + '/' + self.result_dir + '/' + 'preprocessing/fastqc_before_filtering')
                 preprocessing_obj.fastqc_paired_end('fastqc_before_filtering')
-            if self.conf['preprocessing']['trimmomatic']['enable']:
-                preprocessing_obj.trimmomatic_paired_end()
-                self.preprocessing_output_1 = self.result_dir+'/preprocessing/paired_output_1.fastq'
-                self.preprocessing_output_2 = self.result_dir+'/preprocessing/paired_output_2.fastq'
-                if self.conf['preprocessing']['fastqc']['enable']:
-                    preprocessing_obj.input_file_1 = self.preprocessing_output_1
-                    preprocessing_obj.input_file_2 = self.preprocessing_output_2
-                    os.mkdir(os.path.abspath('.') + '/' + self.result_dir + '/' + 'preprocessing/fastqc_after_filtering')
-                    preprocessing_obj.fastqc_paired_end('fastqc_after_filtering')
-            elif self.conf['preprocessing']['cutadapt']['enable']:
-                preprocessing_obj.cutadapt_paired_end()
-                self.preprocessing_output_1 = self.result_dir+'/preprocessing/cutadapt_output_1.fastq'
-                self.preprocessing_output_2 = self.result_dir+'/preprocessing/cutadapt_output_2.fastq'
-                if self.conf['preprocessing']['fastqc']['enable']:
-                    preprocessing_obj.input_file_1 = self.preprocessing_output_1
-                    preprocessing_obj.input_file_2 = self.preprocessing_output_2
-                    os.mkdir(os.path.abspath('.') + '/' + self.result_dir + '/' + 'preprocessing/fastqc_after_filtering')
-                    preprocessing_obj.fastqc_paired_end('fastqc_after_filtering')
-            elif self.conf['preprocessing']['fastp']['enable']:
+            
+            if self.conf['preprocessing']['fastp']['enable']:
                 preprocessing_obj.fastp_paired_end()
                 self.preprocessing_output_1 = self.result_dir+'/preprocessing/fastp_output_1.fastq'
                 self.preprocessing_output_2 = self.result_dir+'/preprocessing/fastp_output_2.fastq'
             else:
-                self.preprocessing_output_1 = self.input_file_1
-                self.preprocessing_output_2 = self.input_file_2
+                if self.conf['preprocessing']['cutadapt']['enable']:
+                    preprocessing_obj.cutadapt_paired_end()
+                    self.preprocessing_output_1 = self.result_dir+'/preprocessing/cutadapt_output_1.fastq'
+                    self.preprocessing_output_2 = self.result_dir+'/preprocessing/cutadapt_output_2.fastq'
+                    if self.conf['preprocessing']['fastqc']['enable']:
+                        preprocessing_obj.input_file_1 = self.preprocessing_output_1
+                        preprocessing_obj.input_file_2 = self.preprocessing_output_2
+                        os.mkdir(os.path.abspath('.') + '/' + self.result_dir + '/' + 'preprocessing/fastqc_after_cutadapt_filtering')
+                        preprocessing_obj.fastqc_paired_end('fastqc_after_cutadapt_filtering')
+                elif self.conf['preprocessing']['trimmomatic']['enable']:
+                    if self.conf['preprocessing']['cutadapt']['enable']:
+                        preprocessing_obj.input_file_1 = self.result_dir+'/preprocessing/cutadapt_output_1.fastq'
+                        preprocessing_obj.input_file_2 = self.result_dir+'/preprocessing/cutadapt_output_2.fastq'
+                    preprocessing_obj.trimmomatic_paired_end()
+                    self.preprocessing_output_1 = self.result_dir+'/preprocessing/paired_output_1.fastq'
+                    self.preprocessing_output_2 = self.result_dir+'/preprocessing/paired_output_2.fastq'
+                    if self.conf['preprocessing']['fastqc']['enable']:
+                        preprocessing_obj.input_file_1 = self.preprocessing_output_1
+                        preprocessing_obj.input_file_2 = self.preprocessing_output_2
+                        os.mkdir(os.path.abspath('.') + '/' + self.result_dir + '/' + 'preprocessing/fastqc_after_trimmomatic_filtering')
+                        preprocessing_obj.fastqc_paired_end('fastqc_after_trimmomatic_filtering')
+                else:
+                    self.preprocessing_output_1 = self.input_file_1
+                    self.preprocessing_output_2 = self.input_file_2
             temp_file = open(os.path.abspath('.') + '/' + self.result_dir+'/Summary_of_results.html', 'a+')
             temp_file.write('<ul>\n')
             temp_file.write('<li>preprocessing result is in %s</li>\n' % './preprocessing')
